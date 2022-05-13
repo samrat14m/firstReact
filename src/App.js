e840ff74771289be1175e6b633a69731b47f0e87
Saurabh Maurya <saurabhmaurya14031997@gmail.com>
2022-05-14 02:16:49 +0530

    div

--- src/App.js
+++ src/App.js
@@ -11,15 +11,15 @@ function App() {
     <div className="App">
       <h1 style={{fontSize:"3vw" , marginLeft:"12vw" , textDecoration:"underline"}}>Style Using Function Component and Class Component</h1>
       <div className="hidden">
-      <header1>  
+      <section>  
       <button onClick={()=>{toggle1?setToggle1(false):setToggle1(true)}}>Made with Functional Component</button>
       {toggle1 && <FunctionalComponent />} 
-      </header1>
+      </section>
 
-      <header2>
+      <section>
       <button onClick={()=>{toggle2?setToggle2(false):setToggle2(true)}} >Made with Class Component</button>
       {toggle2 && <ClassComponent />}
-      </header2>
+      </section>
       </div>
     </div>
   );
